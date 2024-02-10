f521af0ae5ba47102c8c1a7b93b09d98134c8707
CRT_HAO <m831718@gmail.com>
2024-02-11 02:03:17 +0800

    feat: add query method

---
+++ examples/query.rs
@@ -0,0 +1,15 @@
+use q::Q;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let c = Q::new();
+
+    let details = c
+        .query("1122", "AT2005701", "en")
+        .await
+        .expect("failed to query course description");
+
+    println!("{:#?}", details);
+
+    Ok(())
+}
